aefd45a82680fa6a3cd4eddcad05ef84fc37522d
ElishaBartov <elisha.bartov@emerson.com>
2022-11-28 21:20:00 +0200

    Added Joi Validation.

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,13 +1,44 @@
-import { useState } from "react";
+import Joi from "joi";
+import React, { useState } from "react";
 import Title from "../../components/Title";
 import { Offer, data } from "../../data/offers";
 
 function Orders() {
 
+    // States
     const [vacation,setVacation] = useState<string>('');
     const [name,setName] = useState<string>('');
     const [email,setEmail] = useState<string>('');
     const [agree,setAgree] = useState<boolean>(false);
+    const [error,setError] = useState<string>('');
+
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();  // Cancel the default action of the form to reload
+
+        const schema = Joi.object().keys({
+            vacation: Joi.string().required(),
+            name: Joi.string().required().min(2),
+            email: Joi.string().allow(null,'').optional().email({ tlds: {allow: false}}),
+            agree: Joi.boolean().required().equal(true)
+        });
+
+        const { error } = schema.validate({
+            vacation,
+            name,
+            email,
+            agree
+        });
+
+        if(error) {
+            setError(error.message);
+            return;
+        }
+
+        // Clear error state if all inputs are valid
+        setError('');
+
+        console.log("Successfully validated !!!");
+    }
 
     return (
         <>
@@ -18,14 +49,22 @@ function Orders() {
             {
                 data.length > 0 &&
                 <main className="p-5">
-                    <h4 className="mb-3">Billing address</h4>
+                    <h4 className="mb-3">Order Details</h4>
                     <form className="needs-validation">
                         <div className="row g-3">
                             <div className="col-12">
                                 <label className="form-label">
                                     Vacation Package
                                 </label>
-                                <select className="form-select">
+                                <select 
+                                    name="vacation"
+                                    className="form-select"
+                                    value={vacation}
+                                    onChange={(e) => setVacation(e.target.value)}
+                                    >
+                                    <option>
+                                        Please Select
+                                    </option>
                                     {
                                         data.length > 0 && 
                                         data.map((offer:Offer) =>
@@ -77,10 +116,19 @@ function Orders() {
                                 </label>
                             </div>
                             <button
-                                className="w-100 btn btn-primary btn-lg mt-4" type="submit"
+                                className="w-100 btn btn-primary btn-lg mt-4" 
+                                type="submit"
+                                onClick={handleSubmit}
                             >
                                 Continue to checkout
                             </button>
+                            {
+                                // Print error message if inputs are not valid...
+                                error &&
+                                <div className="text-danger">
+                                    {error}
+                                </div>
+                            }
                         </div>
                     </form>
                 </main>
